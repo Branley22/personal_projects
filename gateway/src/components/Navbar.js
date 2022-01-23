724919db8a0ee075bd1c1295480b9e6871a962cf
Branley Minaya <bminaya23@gmail.com>
2022-01-23 06:39:11 -0500

    img src

--- gateway/src/components/Navbar.js
+++ gateway/src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Logo from '../assets/gateway-logo.png';
 
 const Navbar = (props) => {
   return (
     <div className="navbar">
       <div className="left-side">
-        <img src={}/>
+        <img src={Logo}/>
       </div>
       <div className="right-side"></div>
     </div>
---
+++ gateway/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = (props) => {
+  return (
+    <div className="home">
+      Hello world
+    </div>
+  )
+}
+
+export default Home;
