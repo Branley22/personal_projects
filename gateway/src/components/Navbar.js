a3d7bf6b1343e7129ef4a6882542ee7b923d8020
Branley Minaya <bminaya23@gmail.com>
2022-01-22 19:44:46 -0500

    navbar

---
+++ gateway/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = (props) => {
+  return (
+    <div className="navbar">
+      <div className="left-side">
+        <img src={}/>
+      </div>
+      <div className="right-side"></div>
+    </div>
+  )
+}
+
+export default Navbar;
