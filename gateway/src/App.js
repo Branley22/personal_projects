a56dd58f91622581f5866c21a3313ebe74b95b25
Branley Minaya <bminaya23@gmail.com>
2022-01-23 07:32:15 -0500

    css

--- gateway/src/App.js
+++ gateway/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
--- gateway/src/components/Navbar.js
+++ gateway/src/components/Navbar.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Logo from '../assets/gateway-logo.png';
+import { Link } from 'react-router-dom';
+import '../styles/Navbar.css';
 
 const Navbar = (props) => {
   return (
     <div className="navbar">
       <div className="left-side">
-        <img src={Logo}/>
+        <img src={Logo} alt="pic of logo"/>
       </div>
-      <div className="right-side"></div>
+      <div className="right-side">
+        <Link to="/">Home</Link>
+        <Link to="/about-gateway">About Gateway</Link>
+        <Link to="/your-benefits">Your Benefits</Link>
+        <Link to="/popular-tours">Popular Tours</Link>
+        <Link to="/stories">Stories</Link>
+        <Link to="/book-now">Book Now</Link>
+      </div>
     </div>
   )
 }
