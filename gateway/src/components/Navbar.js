4a97e69adf86e8bf06c92384cd563ac55b051296
Branley Minaya <bminaya23@gmail.com>
2022-01-23 19:15:54 -0500

    navbar a css

--- gateway/src/components/Navbar.js
+++ gateway/src/components/Navbar.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../assets/gateway-logo.png';
 import { Link } from 'react-router-dom';
 import ReorderIcon from '@mui/icons-material/Reorder';
 import '../styles/Navbar.css';
 
 const Navbar = (props) => {
+
+  const [openLinks, setOpenLinks] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  };
+
+
   return (
     <div className="navbar">
-      <div className="left-side">
+      <div className="left-side" id={openLinks ? "open" : "close"}>
         <img src={Logo} alt="pic of logo"/>
+        <div className="hidden-links">
+          <Link to="/">Home</Link>
+          <Link to="/about-gateway">About Gateway</Link>
+          <Link to="/your-benefits">Your Benefits</Link>
+          <Link to="/popular-tours">Popular Tours</Link>
+          <Link to="/stories">Stories</Link>
+          <Link to="/book-now">Book Now</Link>
+        </div>
       </div>
       <div className="right-side">
         <Link to="/">Home</Link>
@@ -17,7 +33,7 @@ const Navbar = (props) => {
         <Link to="/popular-tours">Popular Tours</Link>
         <Link to="/stories">Stories</Link>
         <Link to="/book-now">Book Now</Link>
-        <button>
+        <button onClick={toggleNavbar}>
           <ReorderIcon />
         </button>
       </div>
