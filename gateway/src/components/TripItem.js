0bc36eacf48f2bcb794547920113db3eacb40875
Branley Minaya <bminaya23@gmail.com>
2022-02-15 20:12:25 -0500

    tripsitem comp

--- gateway/src/components/TripItem.js
+++ gateway/src/components/TripItem.js
@@ -5,11 +5,16 @@ const TripItem = (props) => {
   const {image, name, price} = props;
 
   return (
-    <div className="trip-item">
-      <div style={{ backgroundImage: `url(${image})` }}></div>
-      <h1>{name}</h1>
-      <p>${price}</p>
-      <button className="trip-item-book">Book now!</button>
+    <div className="trip-box">
+      <div className="trip-box-inner">
+        <div style={{ backgroundImage: `url(${image})` }}></div>
+          <h1>{name}</h1>
+          <p>${price}</p>
+          <button className="trip-item-book">Book now!</button>
+        <div className="trip-box-back">
+          <p>This is the back.</p>
+        </div>
+      </div>
     </div>
   )
 }
