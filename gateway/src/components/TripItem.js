f1cb45f012d7ec4b92c66be698ea6118f70b4d67
Branley Minaya <bminaya23@gmail.com>
2022-02-20 21:14:08 -0500

    flip card trips css

--- gateway/src/components/TripItem.js
+++ gateway/src/components/TripItem.js
@@ -7,10 +7,12 @@ const TripItem = (props) => {
   return (
     <div className="trip-box">
       <div className="trip-box-inner">
-        <div style={{ backgroundImage: `url(${image})` }}></div>
-          <h1>{name}</h1>
-          <p>${price}</p>
-          <button className="trip-item-book">Book now!</button>
+        <div className="trip-box-front">
+          <div style={{ backgroundImage: `url(${image})` }}></div>
+            <h1>{name}</h1>
+            <p>${price}</p>
+        </div>
+        <button className="trip-item-book">Book now!</button>
         <div className="trip-box-back">
           <p>This is the back.</p>
         </div>
