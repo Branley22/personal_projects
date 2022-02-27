4da7f49291bf07da7a70718c205d40084595361e
Branley Minaya <bminaya23@gmail.com>
2022-02-27 18:35:24 -0500

    trip card back svg

--- gateway/src/components/TripItem.js
+++ gateway/src/components/TripItem.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import StarIcon from '@mui/icons-material/Star';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import RestaurantIcon from '@mui/icons-material/Restaurant';
+import WifiIcon from '@mui/icons-material/Wifi';
+import KingBedIcon from '@mui/icons-material/KingBed';
+import ShowerIcon from '@mui/icons-material/Shower';
+import LocalTaxiIcon from '@mui/icons-material/LocalTaxi';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
 
 
 const TripItem = (props) => {
@@ -13,22 +20,25 @@ const TripItem = (props) => {
           <p className="trip-box-days">{numberOfTrips} Day Trip</p>
           <div style={{ backgroundImage: `url(${image})` }}></div>
             <h1>{name}</h1>
-            <h2>{location}</h2>
+            <h2>{location}<LocationOnIcon/></h2>
             <div className="trip-box-rating">
               <p><StarIcon/>{rating}</p>
-              <p>{review} reviews</p>
             </div>
             <p className="trip-box-price"> ${price} / night</p>
         </div>
         <div className="trip-box-back">
+        <p className="trip-box-review">{review} reviews</p>
           <h1>Amentites</h1>
           <h2>What this trip offers</h2>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-          <button className="trip-item-book">Book now!</button>
+          <div className="trip-amentites">
+            <li>Food Included / Room Services<RestaurantIcon/></li>
+            <li>Wifi<WifiIcon/></li>
+            <li>Shower & Bath<ShowerIcon/></li>
+            <li>Discounted Taxi Services<LocalTaxiIcon/></li>
+            <li>Air Conditioning<AcUnitIcon/></li>
+            <button className="amentites-button">Show all 32 amenities</button>
+            <button className="trip-item-book">Book now!</button>
+          </div>
         </div>
       </div>
     </div>
