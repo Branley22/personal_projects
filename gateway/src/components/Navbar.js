4a8eb4e678615ce32728067115c5aede4e7f3bc1
Branley Minaya <bminaya23@gmail.com>
2022-03-02 22:02:41 -0500

    navbardata

--- gateway/src/components/Navbar.js
+++ gateway/src/components/Navbar.js
@@ -2,17 +2,32 @@ import React, { useState } from 'react';
 import Logo from '../assets/gateway-logo.png';
 import { Link } from 'react-router-dom';
 import ReorderIcon from '@mui/icons-material/Reorder';
+import CloseIcon from '@mui/icons-material/Close';
 import '../styles/Navbar.css';
 
 const Navbar = (props) => {
 
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
 
   return (
+    <>
     <div className="navbar">
       <Link to="#" className="menu">
-        <ReorderIcon />
+        <ReorderIcon  onClick={showSidebar}/>
       </Link>
     </div>
+    <nav className={sidebar ? "navbar-menu active" : "navbar-menu"}>
+      <ul className="navbar-items">
+        <li className="navbar-toogle">
+          <Link to="#" className="menu-bars">
+            <CloseIcon />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+    </>
   )
 }
 
--- gateway/src/helpers/NavbarData.js
+++ gateway/src/helpers/NavbarData.js
@@ -1,2 +1,38 @@
+import React from 'react';
+import ReorderIcon from '@mui/icons-material/Reorder';
+import CloseIcon from '@mui/icons-material/Close';
+import HomeIcon from '@mui/icons-material/Home';
 
 
+export const NavbarData = [
+  {
+    title: "Home",
+    path: "/",
+    icon: <HomeIcon />,
+    cName: "nav-text"
+  },
+  {
+    title: "Home",
+    path: "/",
+    icon: <HomeIcon />,
+    cName: "nav-text"
+  },
+  {
+    title: "Home",
+    path: "/",
+    icon: <HomeIcon />,
+    cName: "nav-text"
+  },
+  {
+    title: "Home",
+    path: "/",
+    icon: <HomeIcon />,
+    cName: "nav-text"
+  },
+  {
+    title: "Home",
+    path: "/",
+    icon: <HomeIcon />,
+    cName: "nav-text"
+  }
+]
