3fae69e8073cbd396aea16a62196ddc98f825821
Branley Minaya <bminaya23@gmail.com>
2022-03-08 02:06:35 -0500

    about 1,2,3

--- gateway/src/pages/About.js
+++ gateway/src/pages/About.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import AboutImg from '../assets/aboutimg.png';
+import about1 from '../assets/about1.jpg';
+import about2 from '../assets/about2.jpg';
+import about3 from '../assets/about3.jpg';
 import '../styles/About.css';
 
 const About = (props) => {
@@ -7,14 +9,18 @@ const About = (props) => {
 
   return (
     <div className="about">
-      <div className="about-top"></div>
-      <div className="about-bottom">
+      <div className="about-left">
         <h1>Exciting Trips for you!</h1>
         <h3>Your going to wish you stayed another night</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, or incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut. Cupidatat non proident, sunt in culpa.</p>
         <h3>Enojoy the senroies each trip offers</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, or incididunt ut labore et dolore magna</p>
-      </div>
+        </div>
+        <div className="about-right">
+          <img class="about1-pic" src={about1} width="200px" height="200px"/>
+          <img class="about2-pic" src={about2} width="200px" height="200px"/>
+          <img class="about3-pic" src={about3} width="200px" height="200px"/>
+        </div>
     </div>
   )
 }
