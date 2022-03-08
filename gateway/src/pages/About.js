85ed7deae89653796e45ff71c218b1e065ac41f3
Branley Minaya <bminaya23@gmail.com>
2022-03-07 21:58:18 -0500

    import img

--- gateway/src/pages/About.js
+++ gateway/src/pages/About.js
@@ -7,12 +7,13 @@ const About = (props) => {
 
   return (
     <div className="about">
-      <div className="about-top" style={{ backgroundImage: `url(${AboutImg})` }}></div>
+      <div className="about-top"></div>
       <div className="about-bottom">
-        <h1> ABOUT US</h1>
-        <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+        <h1>Exciting Trips for you!</h1>
+        <h3>Your going to wish you stayed another night</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, or incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut. Cupidatat non proident, sunt in culpa.</p>
+        <h3>Enojoy the senroies each trip offers</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, or incididunt ut labore et dolore magna</p>
       </div>
     </div>
   )
