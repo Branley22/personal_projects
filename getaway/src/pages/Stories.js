af57da6ca4c88b67be78ffaabc6122c7773e64ad
Branley Minaya <bminaya23@gmail.com>
2022-03-19 02:04:19 -0400

    stories pages

--- getaway/src/pages/Stories.js
+++ getaway/src/pages/Stories.js
@@ -8,19 +8,21 @@ const Stories = (props) => {
   return (
     <div className="stories">
       <h1>Genuine Stories</h1>
-      <div className="stories-content">
         <div className="stories-card1">
-          <img class="about1-pic" src={profile1} width="200px" height="200px"/>
-          <h3>I had the best week ever with my husband on our honeymoon</h3>
+        <h3>I had the best week ever with my husband on our honeymoon</h3>
+        <div className="stories-content">
+          <img class="profile-pic1" src={profile1} width="200px" height="200px"/>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-        <div classname="stories-card2">
-          <img class="profile-pic2" src={profile2} width="200px" height="200px"/>
+        </div>
+        </div>
+        <div className="stories-card2">
           <h3>Amazing experience! Going to take more trips with Getaway again soon</h3>
+          <div className="stories-content">
+          <img class="profile-pic2" src={profile2} width="200px" height="200px"/>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
         </div>
         </div>
       </div>
-    </div>
   )
 }
 export default Stories;
