28d73c956162867757b82613646d9cc8b9f46891
Branley Minaya <bminaya23@gmail.com>
2022-03-20 19:49:10 -0400

    stories css responsive

--- getaway/src/pages/Stories.js
+++ getaway/src/pages/Stories.js
@@ -11,19 +11,23 @@ const Stories = (props) => {
         <div className="stories-card1">
           <div className="stories-left">
             <img class="profile-pic" src={profile1} />
+            <p className="stories-comment">Sarah Cameron</p>
+            <span>Jan 23rd, 2017</span>
           </div>
           <div className="stories-right">
             <h3>"I had the best week ever with my husband on our honeymoon"</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
           </div>
         </div>
         <div className="stories-card1">
           <div className="stories-left">
             <img class="profile-pic" src={profile2} />
+            <p className="stories-comment">Sam Wilson</p>
+            <span>Oct 2nd, 2021</span>
           </div>
           <div className="stories-right">
             <h3>"Amazing experience! Going to take more trips with Getaway again soon"</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
           </div>
         </div>
     </div>
