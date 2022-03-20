13751c15c1ebba2484716835a8b6d6586837679a
Branley Minaya <bminaya23@gmail.com>
2022-03-19 23:31:51 -0400

    stories content info

--- getaway/src/pages/Stories.js
+++ getaway/src/pages/Stories.js
@@ -11,8 +11,11 @@ const Stories = (props) => {
         <div className="stories-card1">
         <h3>I had the best week ever with my husband on our honeymoon</h3>
         <div className="stories-content">
-          <img class="profile-pic1" src={profile1} width="200px" height="200px"/>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        <h3>I had the best week ever with my husband on our honeymoon</h3>
+          <div className="stories-content-info">
+            <img class="profile-pic1" src={profile1} width="200px" height="200px"/>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          </div>
         </div>
         </div>
         <div className="stories-card2">
