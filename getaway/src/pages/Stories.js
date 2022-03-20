27069caba6fa0e6875d985377a810580451050c1
Branley Minaya <bminaya23@gmail.com>
2022-03-20 17:41:20 -0400

    stories css

--- getaway/src/pages/Stories.js
+++ getaway/src/pages/Stories.js
@@ -9,22 +9,24 @@ const Stories = (props) => {
     <div className="stories">
       <h1>Genuine Stories</h1>
         <div className="stories-card1">
-        <div className="stories-content">
-        <h3>I had the best week ever with my husband on our honeymoon</h3>
-          <div className="stories-content-info">
-            <img class="profile-pic1" src={profile1} width="200px" height="200px"/>
+          <div className="stories-left">
+            <img class="profile-pic" src={profile1} />
+          </div>
+          <div className="stories-right">
+            <h3>"I had the best week ever with my husband on our honeymoon"</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
           </div>
         </div>
+        <div className="stories-card1">
+          <div className="stories-left">
+            <img class="profile-pic" src={profile2} />
+          </div>
+          <div className="stories-right">
+            <h3>"Amazing experience! Going to take more trips with Getaway again soon"</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          </div>
         </div>
-        <div className="stories-card2">
-          <h3>Amazing experience! Going to take more trips with Getaway again soon</h3>
-          <div className="stories-content">
-          <img class="profile-pic2" src={profile2} width="200px" height="200px"/>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-        </div>
-        </div>
-      </div>
+    </div>
   )
 }
 export default Stories;
