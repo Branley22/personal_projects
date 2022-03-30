021c63a8e460317d0400e96e33752e18922b9a5b
Branley Minaya <bminaya23@gmail.com>
2022-03-30 02:12:32 -0400

    app.js

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,19 +1,62 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
 function App() {
 
-  //const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=d463563c0edeaf3b10adc6fbb19ca1f0`
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=d463563c0edeaf3b10adc6fbb19ca1f0`;
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
+
+
   return (
     <div className="app">
+      <div className="search">
+        <input 
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder="Enter Location"
+        type="text">
+        </input>
+      </div>
       <div className="container">
         <div className="top">
           <div className="location">
-            
+            <p>Dallas</p>
+          </div>
+          <div className="temp">
+            <h1>60°F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
           </div>
         </div>
-        <div className="bottom"></div>
+        <div className="bottom">
+          <div className="feels">
+            <p className="bold">65°F</p>
+            <p>Feels like</p>
+          </div>
+          <div className="humidity">
+            <p className="bold">20%</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className="bold">12 MPH</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
       </div>
     </div>
   );
