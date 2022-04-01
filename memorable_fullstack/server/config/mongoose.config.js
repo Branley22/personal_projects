ab51120128434765bbc2335d5dafa2b0c0b54411
Branley Minaya <bminaya23@gmail.com>
2022-03-31 23:40:41 -0400

    npm server and config file

---
+++ memorable_fullstack/server/config/mongoose.config.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const dbName = "memorable";
+
+mongoose.connect("mongodb://localhost/" + dbName, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+.then (()=> {
+  console.log(`You connected to the ${dbName} database`);
+})
+.catch((err)=> {
+  console.log(`error connecting to the ${dbName} name`);
+  console.log(err);
+})
