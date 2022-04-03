5399e7a0327e694aabf70711b8afc21d57db0a52
Branley Minaya <bminaya23@gmail.com>
2022-04-03 05:18:40 -0400

    model file

--- memorable_fullstack/server/controllers/memorable.controller.js
+++ memorable_fullstack/server/controllers/memorable.controller.js
@@ -3,8 +3,41 @@ const Memorable = require("../models/memorable.model");
 module.exports = {
   findAllMemorables: (req,res) => {
     Memorable.find({})
-    .then((allMemorables) => {
+    .then((allMemorables)=> {
       res.json(allMemorables);
     })
+    .catch((err)=> {
+      console.log("Get all memorables failed");
+      res.status(400).json(err);
+    })
+  },
+
+  fineOneMemorables: (req,res)=> {
+    Memorable.findOne({_id:req.params.id})
+    .then((OneMemorable)=> {
+      res.json(OneMemorable)
+    })
+    .catch((err)=> {
+      res.status(400).json(err)
+    })
+  },
+
+  createNewMemorable: (req,res)=> {
+    Memorable.create(req,body)
+    .then((newMemorable)=> {
+      res.json(newMemorable)
+    })
+    .catch((err)=> {
+      console.log("create memorable failed");
+      res.status(400).json(err)
+    })
+  },
+
+  updatedMemorable: (req,res)=> {
+    Memorable.findOneAndUpdate(
+      {_id:req.params.id},
+      req.body,
+      {new: true, runValidators: true}
+    )
   }
 }
