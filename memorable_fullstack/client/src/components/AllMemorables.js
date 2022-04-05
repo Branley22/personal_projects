8fb88604a83e8e807f698ea956be6b342dc7180a
Branley Minaya <bminaya23@gmail.com>
2022-04-04 21:35:02 -0400

    components

---
+++ memorable_fullstack/client/src/components/AllMemorables.js
@@ -0,0 +1,38 @@
+import React , {useState, useEffect} from 'react';
+import axios from 'axios';
+import {Link, navigate} from '@reach/router';
+
+const AllMemorables = (props) => {
+
+  // const [memorableList, setMemorableList] = useState([]);
+
+  // useEffect(()=> {
+  //   axios.get('http://localhost:8000/api/memorables')
+  //   .then((res)=> {
+  //     console.log(res.data);
+  //     setMemorableList(res.data)
+  //   })
+  //   .catch((err)=> {
+  //     console.log(err);
+  //   })
+  // },[])
+
+
+  return (
+    <div>
+      {/* <h1>Memorables</h1>
+      <div>
+        {
+          memorableList.map((memorable, index)=> (
+            <div key={index}>
+              <p>{memorable.title}</p>
+              <img src={memorable.image} alt= "memorable img"/>
+            </div>
+          ))
+        }
+      </div> */}
+      niljbjnj
+    </div>
+  )
+}
+export default AllMemorables;
