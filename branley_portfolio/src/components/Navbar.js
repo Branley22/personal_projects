4a0a79fd9ca75484edfa7456e02cbe7a9a3d6746
Branley Minaya <bminaya23@gmail.com>
2022-04-05 22:47:46 -0400

    tailwind

--- branley_portfolio/src/components/Navbar.js
+++ branley_portfolio/src/components/Navbar.js
@@ -1,13 +1,39 @@
-import React from 'react'
+import React from 'react';
+import {FaBars, FaTimes} from 'react-icons/fa';
+import Logo from '../assets/logo.png';
 
 const Navbar = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div>
+        <img src={Logo} alt="Logo Image" style={{width: "50px"}} />
+      </div>
+
+      <div>
+        <ul className="flex">
+          <li>Home</li>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Skills</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      
+      <div className="hidden">
+        <FaBars />
+      </div>
+
+      <ul className="hidden">
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Skills</li>
+        <li>Contact</li>        
+      </ul>
+
+      <div className="hidden"></div>
+    </div>
+  )
 }
 
 export default Navbar
-
-
