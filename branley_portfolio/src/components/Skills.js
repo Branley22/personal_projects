f9c997d08d6599f25b6cb0d0686d762651bc1e28
Branley Minaya <bminaya23@gmail.com>
2022-04-07 23:57:06 -0400

    skills

--- branley_portfolio/src/components/Skills.js
+++ branley_portfolio/src/components/Skills.js
@@ -10,14 +10,14 @@ import Github from "../assets/github.png";
 
 const Skills = () => {
   return (
-    <div name="skills" className="bg-[#0a192f] text-gray-300">
-      <div className="max-w-[1000px] max-auto p-4 flex flex-col justify-center w-full h-full">
+    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
           <p className="text-5xl font-bold inline border-b-4 border-blue-400">Skills</p>
           <p className="py-4">// These are the technologies I've worked with!</p>
         </div>
 
-        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-3 text-center py-8">
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
             <p className="my-4">HTML</p>
@@ -39,18 +39,9 @@ const Skills = () => {
             <p className="my-4">MongoDB</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Node} alt="Node icon" />
-            <p className="my-4">Node</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={Tailwind} alt="Tailwind icon" />
             <p className="my-4">Tailwind</p>
           </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Github} alt="Git icon" />
-            <p className="my-4">GitHub</p>
-          </div>
-          
         </div>
       </div>
     </div>
