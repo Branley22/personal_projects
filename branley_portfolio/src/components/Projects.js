7201254ddf14e4697b3c9830cda88747c68c725b
Branley Minaya <bminaya23@gmail.com>
2022-04-10 14:07:58 -0400

    live site goodreads

--- branley_portfolio/src/components/Projects.js
+++ branley_portfolio/src/components/Projects.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import WorkImg from "../assets/workImg.jpeg";
+import Beatmaker from "../assets/beatmaker.jpeg";
+import Getaway from "../assets/getawayapp.jpeg";
+import Goodreads from "../assets/goodreads.jpeg";
+import Mockup from "../assets/mockupapp.jpeg";
+import Movie from "../assets/movieapp.jpeg";
+import Weather from "../assets/weatherapp.jpeg";
 
 const Projects = () => {
   return (
@@ -11,16 +17,16 @@ const Projects = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-          <div style={{backgroundImage: `url(${WorkImg})`}} 
+          <div style={{backgroundImage: `url(${Getaway})`}} 
           className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"> 
 
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                React JS Application Getaway
+                React JS Application
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
+                <a href="https://getawaytrips.netlify.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Live Site</button>
                 </a>
                 <a href="https://github.com/Branley22/Getaway-application">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
@@ -28,12 +34,29 @@ const Projects = () => {
               </div>
             </div>
           </div>
-          <div style={{backgroundImage: `url(${WorkImg})`}} 
+          <div style={{backgroundImage: `url(${Goodreads})`, backgroundRepeat: 'no-repeat', backgroundPosition: "left", backgroundSize: "cover"}} 
           className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"> 
 
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                React JS ApplicationWeather
+              FullStack Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="http://ec2-18-118-45-218.us-east-2.compute.amazonaws.com/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Live Site</button>
+                </a>
+                <a href="https://github.com/Branley22/solo_project">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div style={{backgroundImage: `url(${Weather})`}} 
+          className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"> 
+
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+              React JS Application
               </span>
               <div className="pt-8 text-center">
                 <a href="/">
@@ -45,46 +68,12 @@ const Projects = () => {
               </div>
             </div>
           </div>
-          <div style={{backgroundImage: `url(${WorkImg})`}} 
+          <div style={{backgroundImage: `url(${Beatmaker})`, backgroundRepeat: 'no-repeat', backgroundPosition: "left", backgroundSize: "cover"}} 
           className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"> 
 
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                Full Stack ApplicationBook app
-              </span>
-              <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
-                </a>
-                <a href="https://github.com/Branley22/solo_project">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div style={{backgroundImage: `url(${WorkImg})`}} 
-          className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"> 
-
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Full Stack ApplicationMovie
-              </span>
-              <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
-                </a>
-                <a href="https://github.com/Branley22/mern_group_project">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div style={{backgroundImage: `url(${WorkImg})`}} 
-          className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"> 
-
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                HTML, CSS, JavaScript ApplicationBeatMaker
+              Javascript Application
               </span>
               <div className="pt-8 text-center">
                 <a href="/">
@@ -96,12 +85,29 @@ const Projects = () => {
               </div>
             </div>
           </div>
-          <div style={{backgroundImage: `url(${WorkImg})`}} 
+          <div style={{backgroundImage: `url(${Movie})`, backgroundRepeat: 'no-repeat', backgroundPosition: "15%", backgroundSize: "cover"}} 
           className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"> 
 
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                HTML CSS Application
+                FullStack Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
+                </a>
+                <a href="https://github.com/Branley22/mern_group_project">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div style={{backgroundImage: `url(${Mockup})`, backgroundRepeat: 'no-repeat', backgroundPosition: "right", backgroundSize: "cover"}} 
+          className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"> 
+
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                HTML/CSS Application
               </span>
               <div className="pt-8 text-center">
                 <a href="/">
