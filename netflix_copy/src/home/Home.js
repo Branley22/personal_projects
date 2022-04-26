f26b5f8a4996db5de8f400af1e47d484cd81075e
Branley Minaya <bminaya23@gmail.com>
2022-04-26 19:48:36 -0400

    setup

---
+++ netflix_copy/src/home/Home.js
@@ -0,0 +1,12 @@
+import { AcUnit, AcUnitOutlined } from "@material-ui/icons"
+import "./Home.scss"
+
+const Home = () => {
+  return (
+    <div className="home">
+      <AcUnit />
+    </div>
+  )
+}
+
+export default Home
