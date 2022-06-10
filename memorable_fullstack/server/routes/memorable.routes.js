0d0d505ba035be5b44d05c0816faf8dc323edbd9
Branley Minaya <bminaya23@gmail.com>
2022-06-09 20:23:25 -0400

    change

--- memorable_fullstack/server/routes/memorable.routes.js
+++
@@ -1,9 +0,0 @@
-const MemorableController = require("../controllers/memorable.controller");
-
-module.exports = (app)=> {
-  app.get('/api/memorables', MemorableController.findAllMemorables);
-  app.post('/api/memorables', MemorableController.createNewMemorable);
-  app.get('/api/memorables/:id', MemorableController.fineOneMemorable);
-  app.put('/api/memorables/:id', MemorableController.updatedMemorable);
-  app.delete('/api/memorables/:id', MemorableController.deleteMemorable);
-}
