2a2942b32100fce9c6ef1789c4f3e3407c8a6cf8
Branley Minaya <branleym@Branleys-Air.lan>
2022-10-09 17:45:16 -0400

    remake projects

--- branley_portfolio/src/components/Projects.js
+++ branley_portfolio/src/components/Projects.js
@@ -3,7 +3,7 @@ import Beatmaker from "../assets/beatmaker.jpeg";
 import Getaway from "../assets/getawayapp.jpeg";
 import Goodreads from "../assets/goodreads.jpeg";
 import Mockup from "../assets/mockupapp.jpeg";
-import Movie from "../assets/movieapp.jpeg";
+import Chat from "../assets/chatapp.jpeg";
 import Weather from "../assets/weatherapp.jpeg";
 
 const Projects = () => {
@@ -35,6 +35,23 @@ const Projects = () => {
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Live Site</button>
                 </a>
                 <a href onClick={()=> openInNewTab("https://github.com/Branley22/Getaway-application")}>
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div style={{backgroundImage: `url(${Chat})`, backgroundRepeat: 'no-repeat', backgroundSize: "cover"}} 
+          className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"> 
+
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                FullStack Application
+              </span>
+              <div className="pt-8 text-center">
+                <a href onClick={()=> openInNewTab("https://chat-app2-eb783.web.app/")}>
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Live Site</button>
+                </a>
+                <a href onClick={()=> openInNewTab("https://github.com/Branley22/chat-app")}>
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
                 </a>
               </div>
@@ -86,23 +103,6 @@ const Projects = () => {
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Live Site</button>
                 </a>
                 <a href onClick={()=> openInNewTab("https://github.com/Branley22/personal_projects/tree/master/Beatmaker-project")}>
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div style={{backgroundImage: `url(${Movie})`, backgroundRepeat: 'no-repeat', backgroundPosition: "15%", backgroundSize: "cover"}} 
-          className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"> 
-
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                FullStack Application
-              </span>
-              <div className="pt-8 text-center">
-                <a href onClick={()=> openInNewTab("http://ec2-3-141-5-143.us-east-2.compute.amazonaws.com/")}>
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Live Site</button>
-                </a>
-                <a href onClick={()=> openInNewTab("https://github.com/Branley22/mern_group_project")}>
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
                 </a>
               </div>
